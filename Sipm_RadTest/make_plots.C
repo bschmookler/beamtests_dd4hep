2f53304e4bce7952f7bee602b39d348cdc7982fc
Barak Schmookler <bschmookler1@gmail.com>
2023-06-08 20:08:58 -0700

    Updated SiPM simulation

--- Sipm_RadTest/make_plots.C
+++ Sipm_RadTest/make_plots.C
@@ -26,13 +26,18 @@ void make_plots(){
 	h1a->SetLineColor(kRed);h1a->SetLineWidth(3);
 	h1a->GetXaxis()->SetTitle("Energy deposited in SiPM [MeV]");h1a->GetXaxis()->CenterTitle();
 
+        TH1 *h1b = new TH1D("h1b","For 55 MeV Protons",100,-0.1,6);
+	h1b->SetLineColor(kGreen);h1b->SetLineWidth(3);
+	h1b->GetXaxis()->SetTitle("Energy deposited in SiPM [MeV]");h1b->GetXaxis()->CenterTitle();
+
 	TCanvas *ctemp = new TCanvas("ctemp");
 	t1->Draw("SipmHits.energy*1000.>>h1","fabs(SipmHits.position.z-0.875)<0.01");
 	t1->Draw("SipmHits.energy*1000.>>h1a","fabs(SipmHits.position.z-12.325)<0.01");
+	t1->Draw("SipmHits.energy*1000.>>h1b","fabs(SipmHits.position.z-23.775)<0.01");
 
 	TCanvas *c1 = new TCanvas("c1");
 	c1->SetLogy();
-	h1->Draw();h1a->Draw("same");
+	h1->Draw();h1a->Draw("same");h1b->Draw("same");
 
 	TPaveText* tex1 = new TPaveText(0.65,0.65,0.85,0.85,"NDCNB");
 	tex1->AddText("1^{st} SiPM");tex1->SetTextColor(kBlue);
@@ -43,6 +48,11 @@ void make_plots(){
 	tex1a->AddText("2^{nd} SiPM");tex1a->SetTextColor(kRed);
 	tex1a->SetFillStyle(4000);tex1a->SetTextFont(63);tex1a->SetTextSize(20);
 	tex1a->Draw();
+
+	TPaveText* tex1b = new TPaveText(0.65,0.55,0.85,0.75,"NDCNB");
+	tex1b->AddText("3^{rd} SiPM");tex1b->SetTextColor(kGreen);
+	tex1b->SetFillStyle(4000);tex1b->SetTextFont(63);tex1b->SetTextSize(20);
+	tex1b->Draw();
 
 	//Neutron KE = 1 MeV file
 	TFile *f2 = TFile::Open("Sipm_neutron.edm4hep.root");
